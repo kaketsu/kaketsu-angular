7450fa3d5537b3704eab9fb8659846790236579c
kaketsu <huajiewuxi@gmail.com>
2017-04-28 00:17:39 +0800

    service

--- src/app/take/takeDesk.component.ts
+++ src/app/take/takeDesk.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { MissionService }  from '../service/mission.service';
 
 @Component({
   selector: 'take-desk',
   templateUrl: './takeDesk.component.html',
   styleUrls: ['./takeDesk.component.css']
 })
-export class TakeDeskComponent {}
+export class TakeDeskComponent {
+  astronauts = ['Lovell', 'Swigert', 'Haise'];
+  history: string[] = [];
+  missions = ['Fly to the moon!',
+              'Fly to mars!',
+              'Fly to Vegas!'];
+  nextMission = 0;
+  constructor(private missionService: MissionService) {
+    missionService.missionConfirmed$.subscribe(
+      astronaut => {
+        this.history.push(`${astronaut} confirmed the mission`);
+      });
+  }
+
+  announce() {
+    let mission = this.missions[this.nextMission++];
+    this.missionService.announceMission(mission);
+    this.history.push(`Mission "${mission}" announced`);
+    if (this.nextMission >= this.missions.length) { this.nextMission = 0; }
+  }
+}
